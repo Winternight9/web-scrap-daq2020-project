67867a87cf9bb9205306b13158bb745b66c27e30
Winternight9 <poomiix9@gmail.com>
2020-11-29 21:06:43 +0700

    added calculate duration function and example show value in information page

--- frontend/dota2_guide_react/src/components/infomation.js
+++ frontend/dota2_guide_react/src/components/infomation.js
@@ -6,34 +6,79 @@ const Infomation = (props) => {
     const [heroWinPickRateData, setHeroWinPickRateData] = useState([])
     const [keyOfJsonWinPickRateData, setKeyOfJsonWinPickRateData] = useState([])
     const [keyOfJsonAttributeData, setKeyOfJsonAttributeData] = useState([])
+    const [keyOfDurationEarlyGame, setKeyOfDurationEarlyGame] = useState(0)
+    const [keyOfDurationMidGame, setKeyOfDurationMidGame] = useState(0)
+    const [keyOfDurationLateGame, setKeyOfDurationLateGame] = useState(0)
+    const [heroImage, setHeroImage] = useState("") 
+
     useEffect(() => {
-        async function fetchData(){
+        async function fetchData() {
             const heroRateResponse = await fetch(`http://localhost:1337/characteristics/hero/${props.name}`,{
                 method:'GET',
             })
             const heroAttributeResponse = await fetch(`http://localhost:1337/characters/hero/${props.name}`,{
                 method:'GET',
             })
+            const matchResponse = await fetch('http://localhost:1337/matches',{
+                method:'GET',
+            })
+
+            
             const heroRateJson = await heroRateResponse.json();
             const heroAttributeJson = await heroAttributeResponse.json();
+            const matchJson = await matchResponse.json();
             const heroRateValue = Object.values(heroRateJson[0]).slice(2,12);
             const heroAttributeValue = Object.values(heroAttributeJson[0]).slice(22,38);
             const keyOfHeroRateJson = Object.keys(heroRateJson[0]).slice(2,12);
             const keyOfHeroAttributeJson = Object.keys(heroAttributeJson[0]).slice(22,38);
+
+            let lateGameCount = 0;
+            let earlyGameCount = 0;
+            let midGameCount = 0;
+            
+            for(let i = 0 ; i  < matchJson.length ; i++) {
+                let duration = matchJson[i]['durations'].replace(":","");
+                let matchresult = matchJson[i]['result']
+                let matchWinnerHeroes = matchJson[i][matchresult.toLowerCase()].split(",")
+                
+
+                for(let j = 0; j < 5 ; j++){
+                    if(matchWinnerHeroes[j] == props.name ){
+                        if(parseInt(duration) > 4000 ){
+                            lateGameCount += 1;
+                        }
+                        else if(parseInt(duration) >2500){
+                            midGameCount += 1;
+                        }
+                        else if(parseInt(duration)> 0){
+                            earlyGameCount += 1;
+                        }
+                    break;
+                }
+            }
+            
+            }
+            setKeyOfDurationEarlyGame(earlyGameCount)
+            setKeyOfDurationMidGame(midGameCount)
+            setKeyOfDurationLateGame(lateGameCount)
             setHeroWinPickRateData(heroRateValue)
             setHeroAttributeData(heroAttributeValue)
             setKeyOfJsonWinPickRateData(keyOfHeroRateJson)
             setKeyOfJsonAttributeData(keyOfHeroAttributeJson)
+            setHeroImage(heroAttributeJson[0]['img'])
+
         }
         fetchData()
+
     },[props.name] )
 
     return (
         <div id="specificHero">
+            
            <table id="rateTable">
                 <thead>
                 <tr id="tr_1">
-                    <th></th>
+                    <th><img src= {heroImage}></img></th>
                     <th>{props.name}</th>
                 </tr>
                 </thead>
@@ -70,6 +115,9 @@ const Infomation = (props) => {
                     ))}
                 </tbody>
             </table>
+            {keyOfDurationEarlyGame}
+            {keyOfDurationMidGame}
+            {keyOfDurationLateGame}
         </div>
     )
 }
